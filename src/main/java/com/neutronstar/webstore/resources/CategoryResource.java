7bdd2f3c182c4d8cd28a16625563b5183875c8dd
Ricardo <ricardo.agnello@bol.com.br>
2021-01-17 09:49:37 -0300

    testing first entity - category

--- src/main/java/com/neutronstar/webstore/resources/CategoryResource.java
+++ src/main/java/com/neutronstar/webstore/resources/CategoryResource.java
@@ -1,15 +1,27 @@
 package com.neutronstar.webstore.resources;
 
+import com.neutronstar.webstore.entities.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
     @GetMapping
-    public String listar(){
-        return "REST is OK";
+    public List <Category> listar(){
+
+        Category cat1 = new Category(1L, "Informática");
+        Category cat2 = new Category(2L, "Escritório");
+
+        List<Category> list = new ArrayList<>();
+        list.add(cat1);
+        list.add(cat2);
+        
+        return list;
     }
 }
