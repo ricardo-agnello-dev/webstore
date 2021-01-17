28811b485e248d45f9cf1270e5298e4ccb138bd7
Ricardo <ricardo.agnello@bol.com.br>
2021-01-17 10:41:45 -0300

    H2 database

--- src/main/java/com/neutronstar/webstore/entities/Category.java
+++ src/main/java/com/neutronstar/webstore/entities/Category.java
@@ -3,9 +3,21 @@ package com.neutronstar.webstore.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 
-    private Long id;
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
     private String name;
 
     public Category() {
